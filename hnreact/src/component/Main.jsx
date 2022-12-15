fc789e8026e23e657aae576c3be03a7ca4d0b99a
dmlgus1922 <dmlgus1922@naver.com>
2022-12-15 18:28:46 +0900

    221215 로그인 만들기 전
    로그인 만들기 전..

--- hnreact/src/component/Main.jsx
+++ hnreact/src/component/Main.jsx
@@ -4,6 +4,9 @@ const Main = () => {
   return (
     <div className='main'>
       이것은 메인
+      <h1>로그인</h1>
+      <input type="text" placeholder='아이디를 입력하세요'></input>
+      <input type="password" placeholder='비밀번호를 입력하세요'></input>
     </div>
   )
 }
