143f0e99f0e1f794f0e70f64e01da0f6a877be80
UiHyeon <dmlgus1922@naver.com>
2022-12-18 18:49:57 +0900

    221218 진행

--- hnreact/src/component/Footer.jsx
+++ hnreact/src/component/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../css/Footer.css';
 
 const Footer = () => {
   return (
     <div className='footer'>
       이것은 푸터
+      
     </div>
   )
 }
