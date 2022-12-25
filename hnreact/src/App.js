58826bdcd1716744a25ee6fa930ee25867d3085f
UiHyeon <dmlgus1922@naver.com>
2022-12-25 18:20:23 +0900

    22.12.25 React 공부
    ref에 대해 공부했다.
    클래스형 컴포넌트에 살짝 익숙해지는 것 같다.

--- hnreact/src/App.js
+++ hnreact/src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Header from './component/Header';
@@ -9,27 +10,42 @@ import Counter from './component/study/Counter';
 import Say from './component/study/Say';
 import EventPractice from './component/study/EventPractice';
 import EventPractice2 from './component/study/EventPractice2';
+import ValidationSample from './component/study/ValidationSample';
+import ScrollBox from './component/study/ScrollBox';
 
 import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Header></Header>
-      <Main></Main>
-      <Footer></Footer> */}
-      <EventPractice2></EventPractice2>
-      <EventPractice></EventPractice>
-      <Say></Say>
-      <Counter></Counter>
-      <Test name={'이름'} favoriteNumber={1}>
-        리액트
-        <div>
-          djdasd
-        </div>
-      </Test>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <Header></Header>
+//       <Main></Main>
+//       <Footer></Footer> */}
+//       <ValidationSample></ValidationSample>
+//       <EventPractice2></EventPractice2>
+//       <EventPractice></EventPractice>
+//       <Say></Say>
+//       <Counter></Counter>
+//       <Test name={'이름'} favoriteNumber={1}>
+//         리액트
+//         <div>
+//           djdasd
+//         </div>
+//       </Test>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <ValidationSample></ValidationSample> */}
+        <ScrollBox ref={(ref) => this.scrollBox = ref}></ScrollBox>
+        <button onClick={() => {this.scrollBox.scrollToBottom()}}>맨밑으로 이동</button>
+      </div>
+    );
+  }
 }
 
 export default App;
